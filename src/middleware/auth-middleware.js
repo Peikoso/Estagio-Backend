a2c66687ca72edeb30bdbc277594b00c72fcfd43
João Martins <joaommn1998@gmail.com>
2025-11-25 21:31:00 -0300

    feat: implement public route handling in auth middleware; allow registration without authentication

--- src/middleware/auth-middleware.js
+++ src/middleware/auth-middleware.js
@@ -1,7 +1,19 @@
 import { AuthService } from "../services/auth.js";
 import { UnauthorizedError } from "../utils/errors.js";
 
+const publicRoutes = [
+  { method: 'POST', path: '/api/v1/users/register' }
+];
+
 export const AuthMiddleware = async (req, res, next) => {
+  const isPublic = publicRoutes.some(
+    route => route.method === req.method && req.originalUrl.startsWith(route.path)
+  )
+
+  if(isPublic){
+    return next();
+  }
+
   const authHeader = req.headers.authorization;
 
   if (!authHeader?.startsWith("Bearer ")) {
